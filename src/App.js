5e82024c0e9f716f58a2aee2f7ccf6fdc465ab2e
catkai1 <2250097663@qq.com>
2020-05-03 00:19:37 +0800

    修改了src

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  NavLink,
+  Route
+} from 'react-router-dom'
 import './App.css';
+import Home from './views/Home';
+import Theater from './views/Theater';
+import Wallet from './views/Wallet';
+import My from './views/My';
 
 function App() {
   return (
     <div className="App">
-     是你被
+    <Route path={'/'} exact component={Home}></Route>
+    <Route path={'/Theater'} component={Theater}></Route>
+    <Route path={'/Wallet'} component={Wallet}></Route>
+    <Route path={'/My'} component={My}></Route> 
+    <nav>
+      <NavLink to={'/'} exact>首页</NavLink>
+      <NavLink to={'/Theater'}>剧院</NavLink>
+      <NavLink to={'/Wallet'}>票价</NavLink>
+      <NavLink to={'/My'}>我的</NavLink>
+    </nav>
     </div>
   );
 }
