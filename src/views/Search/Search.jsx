335b9590a7ab394a6821553a49b0f2a7585c578b
lcx <64425606+lcx-123@users.noreply.github.com>
2020-05-05 01:04:48 +0800

    Update Search.jsx

--- src/views/Search/Search.jsx
+++ src/views/Search/Search.jsx
@@ -10,18 +10,16 @@ export default class Demo extends Component {
     render() {
         return (
             <div>
-                <header className={"TopNav"}>
+                <div className={"TopNav"}>
                     <div className={"SearchHead"}>
                         <i className={"iconfont"}>&#xe608;</i>
                         <input type="text" placeholder="搜索热门演出"/>
-                        <i className={"iconfont"} onClick={()=>{
-                            this.state.data=""
-                        }}>&#xe9d8;</i>
+                        <i className={"iconfont"}>&#xe9d8;</i>
                     </div>
                     <span className={""} onClick={()=>{
                         this.props.history.go(-1)
                     }}>取消</span>
-                </header>
+                </div>
             </div>
         )
     }
